eb4db0caf9738de5e6b338af7a6a1bbd8fb9116b
JCabral <cabralzay@gmail.com>
2022-07-25 23:49:50 +0200

    Configurado el POM para empezar el desarrollo

--- src/main/java/hnsp/voll/apiVoll/MainVerticle.java
+++ src/main/java/hnsp/voll/apiVoll/MainVerticle.java
@@ -2,11 +2,25 @@ package hnsp.voll.apiVoll;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
 
 public class MainVerticle extends AbstractVerticle {
 
+  public static void main(String[] args) {
+
+    Vertx.vertx().deployVerticle(new MainVerticle());
+  }
+
+  @Override
+  public void start() { // Promise<Void> startPromise
+    System.out.println(" Init.....");
+  }
+
+
+  /*
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
+
     vertx.createHttpServer().requestHandler(req -> {
       req.response()
         .putHeader("content-type", "text/plain")
@@ -19,5 +33,5 @@ public class MainVerticle extends AbstractVerticle {
         startPromise.fail(http.cause());
       }
     });
-  }
+  }*/
 }
